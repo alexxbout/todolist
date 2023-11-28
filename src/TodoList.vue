276598a639c10bd2e69b0e0e9328c26581667060
alexxbout <boutinaud.pro@gmail.com>
2023-11-28 09:33:20 +0100

    end

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,22 +1,35 @@
 <template>
-    <div ref="containerRef" class="flex flex-col w-screen h-screen p-10 overflow-x-hidden transition-all duration-300 bg-white gap-y-10 scroll-smooth">
+    <div ref="containerRef" class="flex flex-col w-screen h-screen p-5 overflow-x-hidden transition-all duration-300 bg-white md:p-10 gap-y-10 scroll-smooth">
         <div class="flex flex-col w-full border-b gap-y-2">
-            <div class="flex items-center justify-center gap-x-5 w-max">
-                <span class="text-5xl font-bold text-black">Tâches</span>
+            <div class="flex items-center justify-between w-full">
+                <div class="flex items-center justify-center gap-x-5 w-max">
+                    <span class="text-5xl font-bold text-black">Tâches</span>
 
-                <button @click="add" class="flex items-center justify-center duration-300 bg-blue-600 rounded-xl hover:bg-blue-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-white w-9 h-9" viewBox="0 0 16 16">
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                    </svg>
-                </button>
+                    <button @click="add" class="flex items-center justify-center w-10 h-10 duration-300 bg-blue-600 rounded-xl hover:bg-blue-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-white w-9 h-9" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                        </svg>
+                    </button>
+
+                    <select v-model="filter" name="filter" id="filter" class="h-10 px-4 font-medium bg-gray-100 appearance-none rounded-xl">
+                        <option value="0" selected>Toutes</option>
+                        <option value="1">Terminées</option>
+                        <option value="2">En cours</option>
+                    </select>
+                </div>
+
+                <div class="flex items-center justify-center gap-x-5 w-max">
+                    <button @click="removeFinished" class="flex items-center justify-center h-10 px-4 font-medium text-red-600 duration-300 bg-gray-100 rounded-xl hover:bg-gray-300 w-max">Supprimer les tâches terminées</button>
+                    <button @click="removeAll" class="flex items-center justify-center h-10 px-4 font-medium text-white duration-300 bg-red-600 rounded-xl hover:bg-red-800 w-max">Supprimer toutes les tâches</button>
+                </div>
             </div>
 
-            <span class="text-gray-400">{{ completedTodos + " terminée" + (completedTodos > 1 ? "s" : "") }}</span>
+            <span class="text-gray-400">{{ completedTodos.length + " terminée" + (completedTodos.length > 1 ? "s" : "") + " / " + remainingTodos.length + " restante" + (remainingTodos.length > 1 ? "s" : "") }}</span>
         </div>
 
         <div class="flex flex-col gap-y-4">
             <!-- Il est important de placer le v-for à l'extérieur du composant Todo-->
-            <div v-for="todo in list" :key="todo.id">
+            <div v-for="todo in filteredTodos" :key="todo.id">
                 <Todo @@update="handleUpdate" @@remove="handleRemove" :data="todo" />
             </div>
         </div>
@@ -28,9 +41,12 @@ import { computed, ref, watch } from "vue";
 import Todo from "./components/Todo.vue";
 import { ITodo } from "./types/ITodo";
 
-const completedTodos = computed(() => list.value.filter((todo) => todo.completed).length);
+const completedTodos = computed(() => list.value.filter((todo) => todo.completed));
+const remainingTodos = computed(() => list.value.filter((todo) => !todo.completed));
 
 const containerRef = ref<HTMLDivElement>();
+
+const filter = ref<number>(0);
 
 const list = ref<ITodo[]>([
     {
@@ -137,6 +153,10 @@ const list = ref<ITodo[]>([
     },
 ]);
 
+const filteredTodos = computed<ITodo[]>(() => {
+    return filter.value == 0 ? list.value : filter.value == 1 ? completedTodos.value : remainingTodos.value;
+});
+
 const getLastId = (): number => {
     return list.value.length == 0 ? 0 : list.value[list.value.length - 1].id;
 };
@@ -154,6 +174,14 @@ const add = () => {
             if (containerRef.value) containerRef.value.scrollTop = containerRef.value.scrollHeight;
         }, 1);
     }
+};
+
+const removeAll = () => {
+    list.value = [];
+};
+
+const removeFinished = () => {
+    list.value = list.value.filter((todo) => !todo.completed);
 };
 
 const handleRemove = (id: number) => {
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,17 @@
 <template>
-    <div @click="handleClick" class="flex items-center w-full mb-1 border-b h-max gap-x-5">
+    <div @click="handleClick" class="flex items-center w-full p-2 mb-1 transition-colors border-b rounded-md cursor-pointer h-max gap-x-5" :class="isEditing ? 'bg-gray-100' : ''">
         <!-- Checkbox -->
         <label :for="uniqueId" class="relative flex items-center justify-center">
-            <input @change="updateData" v-model="completed" :name="uniqueId" :id="uniqueId" type="checkbox" :class="props.data.completed ? 'border-blue-600' : 'border-gray-400'" class="z-10 peer aspect-square w-5 text-blue-600 bg-tr border-[1px] appearance-none focus:ring-blue-500 rounded-full focus:ring-2" />
+            <input @change="toggle" v-model="completed" :name="uniqueId" :id="uniqueId" type="checkbox" :class="props.data.completed ? 'border-blue-600' : 'border-gray-400'" class="peer aspect-square w-5 text-blue-600 bg-tr border-[1px] appearance-none focus:ring-blue-500 rounded-full focus:ring-2" />
 
-            <span class="z-0 absolute bg-blue-600 rounded-full w-max aspect-square p-[6px] peer-checked:inline hidden"></span>
+            <span class="absolute bg-blue-600 rounded-full w-max aspect-square p-[6px] peer-checked:inline hidden"></span>
         </label>
 
         <!-- Title and date -->
-        <div class="flex flex-col w-full gap-y-2">
-            <input @change="updateData" ref="titleRef" v-model="title" type="text" class="text-xl font-medium text-black bg-transparent outline-none appearance-none" :class="props.data.completed ? 'line-through text-gray-400' : 'text-black'" />
+        <div class="flex flex-col w-full gap-y-2 max-w-[100%]">
+            <input @blur="isEditing = false" @change="updateData" ref="titleRef" v-model="title" type="text" class="px-1 text-xl font-medium bg-transparent outline-none appearance-none max-w-screen w-60" :class="props.data.completed ? 'line-through text-gray-400' : isEditing ? 'text-blue-600' : 'text-black'" :disabled="!isEditing" />
 
-            <input @change="updateData" ref="dateRef" v-model="date" type="datetime-local" class="text-gray-400 bg-transparent outline-none appearance-none w-44" />
+            <input ref="dateRef" v-model="date" type="datetime-local" class="text-gray-400 bg-transparent outline-none appearance-none w-44" />
         </div>
 
         <!-- Delete button -->
@@ -20,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import { PropType, ref, watch } from "vue";
+import { PropType, ref } from "vue";
 import { ITodo } from "../types/ITodo";
 
 const props = defineProps({
@@ -39,8 +39,11 @@ const date = ref<string>(props.data.endDate.toISOString().slice(0, 16));
 const titleRef = ref<HTMLInputElement>();
 const dateRef = ref<HTMLInputElement>();
 
+const isEditing = ref<boolean>(false);
+
 let clickCount = 0;
 let lastClickTime = 0;
+
 
 const handleClick = () => {
     const now = Date.now();
@@ -48,6 +51,20 @@ const handleClick = () => {
 
     if (now - lastClickTime < DOUBLE_CLICK_DELAY) {
         // Double click
+        if (!completed.value) {
+            isEditing.value = !isEditing.value;
+
+            if (isEditing.value) {
+                setTimeout(() => {
+                    titleRef.value?.focus();
+
+                    // Set caret position to the end of the input
+                    if (titleRef.value) {
+                        titleRef.value.selectionStart = titleRef.value.selectionEnd = titleRef.value.value.length;
+                    }
+                }, 0);
+            }
+        }
     } else {
         // Simple click
         clickCount++;
@@ -68,6 +85,20 @@ const remove = () => {
     emits("@remove", props.data.id);
 };
 
+const toggle = () => {
+    // Disable the title and date if the todo is completed
+    if (titleRef.value && dateRef.value) {
+        titleRef.value.disabled = completed.value;
+        dateRef.value.disabled = completed.value;
+
+        if (completed.value) {
+            isEditing.value = false;
+        }
+    }
+
+    updateData();
+};
+
 const updateData = () => {
     // Check if string is empty and emit remove event if it is
     if (title.value.trim() === "") {
@@ -77,14 +108,14 @@ const updateData = () => {
     }
 };
 
-watch(
-    () => completed.value,
-    (value) => {
-        // Disable the title and date if the todo is completed
-        if (titleRef.value && dateRef.value) {
-            titleRef.value.disabled = value;
-            dateRef.value.disabled = value;
-        }
-    }
-);
+// watch(
+//     () => completed.value,
+//     (value) => {
+//         // Disable the title and date if the todo is completed
+//         if (titleRef.value && dateRef.value) {
+//             titleRef.value.disabled = value;
+//             dateRef.value.disabled = value;
+//         }
+//     }
+// );
 </script>
