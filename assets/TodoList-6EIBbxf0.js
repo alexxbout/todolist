c9296eefd8a02bd0428259eac670e8468a79f0f7
alexxbout <boutinaud.pro@gmail.com>
2023-11-29 10:36:07 +0100

    Updates

---
+++ assets/TodoList-6EIBbxf0.js
@@ -0,0 +1,1 @@
+import{_ as f}from"./index-sTRCSP6F.js";export{f as default};
--- src/types/ITodo.ts
+++
@@ -1,6 +0,0 @@
-export interface ITodo {
-    title: string;
-    completed: boolean;
-    endDate: Date;
-    id: number;
-}
